e3940b67312263d8a23cf5e9f1534891d7ac486b
Cameron Dodd <cameronradodd@gmail.com>
2025-04-17 16:03:41 -0700

    Added "start game"

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public GameObject pogoStick, mainMenuObject;
-
+    public GameObject pogoStick, mainMenuObject, cameraObject;
     void Start(){
         pogoStick.GetComponent<Rigidbody>().isKinematic = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            pogoStick.GetComponent<Rigidbody>().isKinematic = false;
             mainMenuObject.SetActive(false);
             pogoStick.GetComponent<RotateOnY>().enabled = false;
+            pogoStick.transform.DORotate(new Vector3(0, 0, 0), 2.0f);
+            pogoStick.transform.DOMove(cameraObject.transform.position + new Vector3(0, -4, 8), 2.0f).OnComplete(() => {
+                cameraObject.transform.parent = pogoStick.transform;
+                pogoStick.GetComponent<Rigidbody>().isKinematic = false;
+            });
         }
     }
 }
